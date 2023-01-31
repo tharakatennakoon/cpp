4dbf7880128518a472275f597989414c1a24bbaf
tharakatennakoon <tharakatennakoon@gmail.com>
2023-01-31 14:02:41 +0000

    change to c++ 17 as leetcode uses 17

--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,51 @@
 #include <string>
 #include <type_traits>
 
-void TestLabmda() {
-    auto l = [](auto arg) {
-        std::cout << arg << ' ';
+// void TestLabmda() {
+//     auto l = [](auto arg) {
+//         std::cout << arg << ' ';
 
-        return std::type_identity<void>{};
-    };
+//         return std::type_identity<void>{};
+//     };
 
-    auto log = [&l] (auto... args) {
-        (l(args) = ...);
-    };
+//     auto log = [&l] (auto... args) {
+//         (l(args) = ...);
+//     };
 
-    log(1,"2",3.0);
-}
+//     log(1,"2",3.0);
+// }
 
-void TestVariant() {
-    std::variant<int, float> v,w;
+// void TestVariant() {
+//     std::variant<int, float> v,w;
 
-    v = 42;
+//     v = 42;
 
-    try
-    {
-        int i = std::get<int>(v);
-        std::cout << "I : " << i << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+//     try
+//     {
+//         int i = std::get<int>(v);
+//         std::cout << "I : " << i << std::endl;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
     
-    try
-    {
-        float f = std::get<float>(v);
-        std::cout << "F : " << f << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-}
+//     try
+//     {
+//         float f = std::get<float>(v);
+//         std::cout << "F : " << f << std::endl;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+// }
 
 int main()
 {
-    TestVariant();
+    //TestVariant();
 
-    TestLabmda();
+    //TestLabmda();
 
     return 0;
 }

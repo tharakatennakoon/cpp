3a21efabd66249eecd18a258805d32c3fd9aa723
Tharaka Tennakoon <btnv78@zebra.com>
2023-08-18 18:52:38 +0530

    restructure

--- src/OpenGL/main.cpp
+++ src/OpenGL/main.cpp
@@ -10,6 +10,80 @@ std::string get_file_string(std::string filePath)
                        (std::istreambuf_iterator<char>()));
 }
 
+void windowSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+GLuint createShaderProgram()
+{
+    std::string strVert = get_file_string("./shader/triangle.vert");
+    const GLchar *vertCStr = strVert.c_str();
+    std::cout << strVert.c_str() << std::endl;
+
+    std::string strFrag = get_file_string("./shader/triangle.frag");
+    const GLchar *fragCStr = strFrag.c_str();
+    std::cout << strFrag.c_str() << std::endl;
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertCStr, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragCStr, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+GLuint copyVertexDataToGPU()
+{
+    GLfloat verts[] = {
+        +0.0f, +0.0f, 0.0f,        // Location XYZ
+        +1.0f, +0.0f, 0.0f, +1.0f, // Color RGBA
+        +1.0f, +1.0f, 0.0f,
+        +1.0f, +0.0f, 0.0f, +1.0f,
+        -1.0f, +1.0f, 0.0f,
+        +1.0f, +0.0f, 0.0f, +1.0f,
+
+        -1.0f, -1.0f, 0.0f,
+        +1.0f, +0.0f, 0.0f, +1.0f,
+        +1.0f, -1.0f, 0.0f,
+        +1.0f, +0.0f, 0.0f, +1.0f};
+
+    GLuint vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)(sizeof(GLfloat) * 3));
+
+    return vertexBufferObject;
+}
+
+GLuint copyElementDataToGPU()
+{
+    GLushort indices[] = {0, 1, 2, 0, 3, 4};
+    GLuint indexBufferObject;
+    glGenBuffers(1, &indexBufferObject);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    return indexBufferObject;
+}
+
 int main()
 {
     std::cout << "Hello world" << std::endl;
@@ -30,51 +104,14 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(window, windowSizeCallback);
 
     gladLoadGL();
 
-    std::string strVert = get_file_string("./shader/triangle.vert");
-    const GLchar *vertCStr = strVert.c_str();
-    std::cout << strVert.c_str() << std::endl;
-
-    std::string strFrag = get_file_string("./shader/triangle.frag");
-    const GLchar *fragCStr = strFrag.c_str();
-    std::cout << strFrag.c_str() << std::endl;
-
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertCStr, NULL);
-    glCompileShader(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragCStr, NULL);
-    glCompileShader(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-    //    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    GLfloat verts[] = {
-        +0.0f, +1.0f, 0.0f,
-        -1.0f, -1.0f, 0.0f,
-        +1.0f, -1.0f, 0.0f};
-
-    GLuint VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(GLfloat), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    GLuint vertexBufferObject = copyVertexDataToGPU();
+    GLuint indexBufferObject = copyElementDataToGPU();
+    GLuint shaderProgram = createShaderProgram();
+    glUseProgram(shaderProgram);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -84,10 +121,8 @@ int main()
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 6); // Use element array for
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -96,8 +131,8 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &vertexBufferObject);
+    glDeleteBuffers(1, &indexBufferObject);
     glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();

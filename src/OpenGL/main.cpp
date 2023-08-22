5d45e5e660223cca8819536226c5fc6ad02558c3
Tharaka Tennakoon <btnv78@zebra.com>
2023-08-22 13:58:59 +0530

    change to glad functions and add depth test

--- src/OpenGL/main.cpp
+++ src/OpenGL/main.cpp
@@ -12,7 +12,7 @@ std::string get_file_string(std::string filePath)
 
 void windowSizeCallback(GLFWwindow *window, int width, int height)
 {
-    glViewport(0, 0, width, height);
+    glad_glViewport(0, 0, width, height);
 }
 
 GLuint createShaderProgram()
@@ -25,61 +25,66 @@ GLuint createShaderProgram()
     const GLchar *fragCStr = strFrag.c_str();
     std::cout << strFrag.c_str() << std::endl;
 
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertCStr, NULL);
-    glCompileShader(vertexShader);
+    GLuint vertexShader = glad_glCreateShader(GL_VERTEX_SHADER);
+    glad_glShaderSource(vertexShader, 1, &vertCStr, NULL);
+    glad_glCompileShader(vertexShader);
 
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragCStr, NULL);
-    glCompileShader(fragmentShader);
+    GLuint fragmentShader = glad_glCreateShader(GL_FRAGMENT_SHADER);
+    glad_glShaderSource(fragmentShader, 1, &fragCStr, NULL);
+    glad_glCompileShader(fragmentShader);
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    GLuint shaderProgram = glad_glCreateProgram();
+    glad_glAttachShader(shaderProgram, vertexShader);
+    glad_glAttachShader(shaderProgram, fragmentShader);
+    glad_glLinkProgram(shaderProgram);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glad_glDeleteShader(vertexShader);
+    glad_glDeleteShader(fragmentShader);
 
     return shaderProgram;
 }
 
 GLuint copyVertexDataToGPU()
 {
+    constexpr float first_trianlge_depth = 0.5;
+    constexpr float second_trianlge_depth = -0.5;
+
     GLfloat verts[] = {
-        +0.0f, +0.0f, 0.0f,        // Location XYZ
+        +0.0f, +1.0f, -1,          // Location XYZ
         +1.0f, +0.0f, 0.0f, +1.0f, // Color RGBA
-        +1.0f, +1.0f, 0.0f,
+        -1.0f, -1.0f, first_trianlge_depth,
         +0.0f, +1.0f, 0.0f, +1.0f,
-        -1.0f, +1.0f, 0.0f,
+        +1.0f, -1.0f, first_trianlge_depth,
         +0.0f, +0.0f, 1.0f, +1.0f,
 
-        -1.0f, -1.0f, 0.0f,
-        +1.0f, +1.0f, 0.0f, +1.0f,
-        +1.0f, -1.0f, 0.0f,
-        +1.0f, +0.0f, 1.0f, +1.0f};
+        -1.0f, +1.0f, second_trianlge_depth, // Location XYZ
+        +1.0f, +1.0f, 0.0f, +1.0f,           // Color RGBA
+        +0.0f, -1.0f, second_trianlge_depth,
+        +1.0f, +0.0f, 1.0f, +1.0f,
+        +1.0f, +1.0f, second_trianlge_depth,
+        +0.0f, +1.0f, 1.0f, +1.0f};
 
     GLuint vertexBufferObject;
-    glGenBuffers(1, &vertexBufferObject);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glad_glGenBuffers(1, &vertexBufferObject);
+    glad_glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glad_glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)0);
+    glad_glEnableVertexAttribArray(0);
+    glad_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)0);
 
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)(sizeof(GLfloat) * 3));
+    glad_glEnableVertexAttribArray(1);
+    glad_glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void *)(sizeof(GLfloat) * 3));
 
     return vertexBufferObject;
 }
 
 GLuint copyElementDataToGPU()
 {
-    GLushort indices[] = {0, 1, 2, 0, 3, 4};
+    GLushort indices[] = {0, 1, 2, 3, 4, 5};
     GLuint indexBufferObject;
-    glGenBuffers(1, &indexBufferObject);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glad_glGenBuffers(1, &indexBufferObject);
+    glad_glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+    glad_glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     return indexBufferObject;
 }
@@ -107,6 +112,7 @@ int main()
     glfwSetWindowSizeCallback(window, windowSizeCallback);
 
     gladLoadGL();
+    glad_glEnable(GL_DEPTH_TEST);
 
     GLuint vertexBufferObject = copyVertexDataToGPU();
     GLuint indexBufferObject = copyElementDataToGPU();
@@ -117,12 +123,13 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glad_glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        glad_glClear(GL_COLOR_BUFFER_BIT);
+        glad_glClear(GL_DEPTH_BUFFER_BIT);
 
         // glDrawArrays(GL_TRIANGLES, 0, 6); // Use element array for
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+        glad_glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -131,9 +138,9 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteBuffers(1, &vertexBufferObject);
-    glDeleteBuffers(1, &indexBufferObject);
-    glDeleteProgram(shaderProgram);
+    glad_glDeleteBuffers(1, &vertexBufferObject);
+    glad_glDeleteBuffers(1, &indexBufferObject);
+    glad_glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
